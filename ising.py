62b94355b2c9eb42f94bf1be64296b62dfddad07
Bowie <ianlopes2069@gmail.com>
2024-09-01 16:24:14 -0300

    código final usado no tcc

--- ising.py
+++ ising.py
@@ -18,8 +18,6 @@ import time
 
 #============================================================================================
 #                                DEFININDO PARÂMETROS
-
-inicio = time.time()
 l = 8 #dimensão da rede 
 num_dir = 4 #número de vizinhos
 t = 1.0 #temperatura inicial
@@ -39,25 +37,11 @@ def inicialize_rede(L): #Cria rede de spins aleatórios
 
 
 @njit()
-def metropolis(mcs, matriz, T, L):
-    #cont_aceite = 0
-    #cont_total = 0
-    energia_total = 0
-    for y in range(L):
-        for x in range(L):
-            for _ in range(num_dir):
-                i , vizinhos = vizinho(matriz, L, y, x)
-                energia_total += (matriz[y][x] * vizinhos[_])
-    energia_total = -energia_total / 2
+def metropolis(mcs, matriz, T, L, energia_total, magnetizacao): #Executa o Algoritmo de Metropolis
     energia_autal = energia_total
     sum_energia = 0
     sum_energia_quadrada = 0
-    cont = 0
-    magnetizçao = 0
-    for i in range(L):
-        for j in range(L):
-            magnetizçao += matriz[i][j]
-    magnetizaçao_atual = magnetizçao
+    magnetizaçao_atual = magnetizacao
     sum_magnetizaçao = 0
     sum_magnetizaçao_quadrada = 0
     for i in range(mcs): #Loop sobre o Passo Monte Carlo
@@ -68,43 +52,35 @@ def metropolis(mcs, matriz, T, L):
             delta_M = calcular_variação_magnetizaçao(matriz, y, x)
             if delta_E > 0:
                 q = np.random.random() #Número aleatório entre 0 e 1 
-                prob = np.exp(-delta_E/ T)
+                prob = np.exp(-delta_E/ T) #Calcula a probabilidade de Boltzmann
                 if q < prob:
-                    matriz[y][x] = (matriz[y][x]) * -1 
-                    #cont_aceite += 1
-                    energia_autal += delta_E
-                    magnetizaçao_atual += delta_M
-                #print(f'q = {q} e prob = {prob}')
+                    matriz[y][x] = (matriz[y][x]) * -1 #Inverto o spin
+                    energia_autal += delta_E #Atualiza a energia interna
+                    magnetizaçao_atual += delta_M #Atualiza a magnetização
             else:
-                matriz[y][x] = (matriz[y][x]) * -1 
-                #cont_aceite += 1
-                energia_autal += delta_E
-                magnetizaçao_atual += delta_M
-            #cont_total += 1
-            cont += 1
-            sum_energia += energia_autal
-            sum_energia_quadrada += energia_autal * energia_autal
-            sum_magnetizaçao += magnetizaçao_atual
-       # if (i % 500 == 0):
-    e_m = sum_energia/cont
-    e_m_q = sum_energia_quadrada/cont
-    m_m = sum_magnetizaçao/cont
-    """sum_energia_quadrada += energia_autal * energia_autal
-    sum_magnetizaçao += magnetizaçao_atual
-    sum_magnetizaçao_quadrada += magnetizaçao_atual * magnetizaçao_atual"""
-    #energia_total = energia_autal
-    return matriz, e_m, energia_autal, e_m_q, m_m, cont, sum_energia
+                matriz[y][x] = (matriz[y][x]) * -1 #Inverto o spin
+                energia_autal += delta_E #Atualiza a energia interna
+                magnetizaçao_atual += delta_M #Atualiza a magnetização
+            sum_energia += energia_autal #Atualiza a soma da energia interna
+            sum_energia_quadrada += energia_autal * energia_autal #Atualiza a soma da energia interna quadrática
+            sum_magnetizaçao += magnetizaçao_atual #Atualiza a soma da magnetização
+            sum_magnetizaçao_quadrada += magnetizaçao_atual * magnetizaçao_atual #Atualiza a soma da magnetização quadrática
+    e_m = sum_energia/(mcs * (L**2)) #Calcula a energia interna média
+    e_m_q = sum_energia_quadrada/(mcs * (L**2)) #Calcula a energia interna mádia quadrática
+    m_m = sum_magnetizaçao/(mcs * (L**2)) #Calcula a magnetização média
+    m_m_q = sum_magnetizaçao_quadrada/(mcs * (L**2)) #Calcula a magnetização média quadrática
+    return matriz, e_m, energia_autal, e_m_q, m_m, sum_energia, magnetizaçao_atual, m_m_q
 
 
 @njit()
-def calcular_variação_energia(matriz, L, y, x): 
+def calcular_variação_energia(matriz, L, y, x): #Calcula a variação da energia cada vez que um novo sítio aleatório é escolhido
     sum_vizinhos, _ = vizinho(matriz, L, y, x)
     variacao_energia = 2 * matriz[y][x] * sum_vizinhos
     return variacao_energia
 
 
 @njit()
-def calcular_variação_magnetizaçao(matriz, y, x):
+def calcular_variação_magnetizaçao(matriz, y, x): #Calcula a variação da magnetização cada vez que um novo sítio aleatório é escolhido
     variação_magnetizaçao = -2 * matriz[y][x]
     return variação_magnetizaçao
     
@@ -117,16 +93,13 @@ def vizinho(matriz, L, y, x): #Condições de contorno periódica
     vizinhos.append(matriz[y-1][x]) # primeiro vizinho de cima
     vizinhos.append(matriz[y][x-1]) # primeiro vizinho da esquerda
     vizinhos.append(matriz[(y+1) % L][x]) #primeiro vizinho de baixo
-    #vizinhos = np.array(r)
-    #print(vizinhos)
     sum_vizinhos = 0
     for _ in range(4): #Soma sobre os vizinhos
         sum_vizinhos += vizinhos[_] 
-        #print(n_vizinhos)
     return sum_vizinhos, vizinhos
 
 
-"""def energia_tot(matriz, L, num_dir):
+def energia_tot(matriz, L, num_dir): #Calcula a energia interna total
     energia_total = 0
     for y in range(L):
         for x in range(L):
@@ -134,66 +107,45 @@ def vizinho(matriz, L, y, x): #Condições de contorno periódica
                 i , vizinhos = vizinho(matriz, L, y, x)
                 energia_total += (matriz[y][x] * vizinhos[_])
     energia_total = -energia_total / 2 #Dividido por 2 dois porque cada sítio é contado duas vezes
-    return energia_total"""
+    return energia_total
 
 
-"""def magnetizçao_total(matriz, L):
+def magnetizçao_total(matriz, L): #Calcula a manetização total
     magnetizçao = 0
     for i in range(L):
         for j in range(L):
             magnetizçao += matriz[i][j]
     return magnetizçao
-"""
 
-"""#@njit
-def resultados( cont, sum_energia, sum_energia_quadrada, L, T, sum_magnetizaçao, sum_magnetizaçao_quadrada):
-    energia_media = sum_energia/cont
-    energia_media_quadrada = sum_energia_quadrada/cont
-    magnetizçao_media = sum_magnetizaçao/cont
-    magnetizçao_media_quadrada = sum_magnetizaçao_quadrada/cont
-    calor_especifico = (energia_media_quadrada - (energia_media * energia_media))/(L * L * T *T) #Calor específico por sítio
-    susceptibilidade_magnetica = (magnetizçao_media_quadrada -(magnetizçao_media * magnetizçao_media))/(L * L * T) #Susceptibilidade magnética por sítio"""
 
 #===================================================================================================================
 #                                          PROGRAMA PRINCIPAL
-inicio = time.time()
 matriz = np.array(inicialize_rede(l))
-#print(f"initialize demorou {time.time() - inicio:.2f} segundos")
-#print(f'Matriz inicial: {matriz}')
-inicio = time.time()
-#energia_total = energia_tot(matriz, l, num_dir)
-#print(f"energia_tot demorou {time.time() - inicio:.2f} segundos")
-inicio = time.time()
-#magnetizçao_t = magnetizçao_total(matriz, l) #tirar função desnecessária
-#print(f"magnetizacao demorou {time.time() - inicio:.2f} segundos")
-inicio = time.time()
-matriz, _, _, _, _, _, _ = metropolis(mcs, matriz, t, l) #Executa o Algoritmo de Metropolis para a equilibração do sistema
-print(f"metropolis demorou {time.time() - inicio:.2f} segundos")
+energia_total = energia_tot(matriz, l, num_dir)
+magnetizcao = magnetizçao_total(matriz, l)
+matriz, _, energia_total, _, _, _, magnetizcao, _ = metropolis(mcs, matriz, t, l, energia_total, magnetizcao) #Executa o Algoritmo de Metropolis para a equilibração do sistema
 matriz = np.array(matriz)
 energia_media_ps = []
 m_m_ps = []
 calor_esp = []
+susceptibilidade_magnetica = []
 t_p = []
 inicio = time.time()
 for k in range(range_temp): #Loop sobre as temperaturas
     t_p.append(t)
-    matriz, e_m, energia_totall, e_m_q, m_m, cont, sum_e = metropolis(mcs, matriz, t, l) #Executa o algoritmo para calcular as propriedades
+    matriz, e_m, energia_total, e_m_q, m_m, sum_e, magnetizcao, m_m_q = metropolis(mcs, matriz, t, l, energia_total, magnetizcao) #Executa o algoritmo para calcular as propriedades
     matriz = np.array(matriz)
-    e = sum_e/cont
-    energia_total = energia_totall
-    calor = ((e_m_q - (e_m * e_m))/( l * l * t * t))
-    calor_esp.append(calor) #Calor específico por sítio
-    t = t + passo_temp
-    energia_media_ps.append(e/(l * l))
-    m_m_ps.append((m_m)/(l * l))
-print(energia_total)
-fim = time.time()
-print(fim - inicio)
+    t = t + passo_temp #Incrementa a temperatura
+    energia_media_ps.append(e_m/(l * l)) #Energia média por sítio
+    calor_esp.append((e_m_q - (e_m * e_m))/( l * l * t * t)) #Calor específico por sítio
+    m_m_ps.append((m_m)/(l * l)) #Magnetização média por sítio
+    susceptibilidade_magnetica.append((m_m_q -(m_m * m_m))/(l * l * t)) #Susceptibilidade magnética por sítio
 plt.title('magnetização média por sítio em funcão da tempertura')
 plt.xlabel('T')
 plt.ylabel('m/N')
-#plt.scatter(t_p, energia_media_ps)
-#plt.scatter(t_p, calor_esp)
+plt.scatter(t_p, energia_media_ps)
+plt.scatter(t_p, calor_esp)
 plt.scatter(t_p,m_m_ps)
+plt.scatter(t_p,susceptibilidade_magnetica)
 plt.show()
 #=====================================================================================================================
